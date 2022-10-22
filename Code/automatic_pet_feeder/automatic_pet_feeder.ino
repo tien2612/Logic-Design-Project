8aa1099e28b9e5e79271abacdc197d178c482531
Nguyen Phuc Tien <nguyenphuctien1258@gmail.com>
2022-10-22 22:45:32 +0700

    updated max display time & set max food per day

--- Code/automatic_pet_feeder/automatic_pet_feeder.ino
+++ Code/automatic_pet_feeder/automatic_pet_feeder.ino
@@ -1,27 +1,37 @@
-//YWROBOT
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
+// AUTOMATIC PET FEEDER
+// HOW TO USE?
+// Button0: Press once to switch between food remains and time to feed information.
+// Button1: Setup time to feed your pet.
+// Button2: Set the maximum amount of food to feed per day (it can only be set to 1600g). 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <OneButton.h>
 
 #define IS_PRESSED  HIGH
 #define R_MODE      1     // display remaining food on LCD
 #define T_MODE      2     // display time to feed on LCD
+#define INIT_MODE   4
 #define H_MODE      5     // Set schedule for hour
 #define M_MODE      6     // Set schedule for minute
 #define S_MODE      7     // Set schedule for second
+#define TIME_NOTHING_CHANGES  10000
+OneButton btn1(51, true);
+unsigned long startMillis;  // Some global variables available anywhere in the program
+unsigned long currentMillis; // Current time since the program started 
 
-int MAX_FOOD_PER_DAY = 0;
-int remaining_food = 0;
-int btn[2] = {53, 51};
-int hour = 0, minute = 0, second = 0;
+int MAX_FOOD_PER_DAY;
+int remaining_food;
+int btn[3] = {53, 51, 49};
+int hour, minute, second;
 int mode = R_MODE;
-int counter = 0;
-int status = H_MODE;
+int counter;
+int status;
 extern volatile unsigned long timer0_millis;
+int flag_longclick_btn1;
+int flag_settingSchedule;
 
 LiquidCrystal_I2C lcd(0x20,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
+// Display the remaining food on the LCD. 
 void displayRemainingFood_LCD() {
     lcd.init();                      // initialize the lcd 
     // Print a message to the LCD.
@@ -41,8 +51,9 @@ void displayRemainingFood_LCD() {
     lcd.setCursor(current_cursor, 1);
     lcd.print(remaining_food);
     lcd.setCursor(current_cursor + count + 1, 1);
-    lcd.print("KG");
+    lcd.print("G");
 }
+// Display feed time on LCD 
 void displayTimeSchedule_LCD(int hour, int minute, int second) {
     int current_cursor = 0;
     lcd.init();                      // initialize the lcd 
@@ -50,7 +61,23 @@ void displayTimeSchedule_LCD(int hour, int minute, int second) {
     lcd.clear();                    // Clear screen
     lcd.backlight();                // Turn on background light
     lcd.setCursor(2,0);
-    lcd.print("TIME TO FEED");
+    if (flag_settingSchedule) {
+        switch(status) {
+            case H_MODE:
+                 lcd.print("SET HOUR");
+                 break;
+            case M_MODE:
+                 lcd.print("SET MINUTE");
+                 break;
+            case S_MODE:
+                 lcd.print("SET SECOND");
+                 break;  
+            default:
+                 lcd.print("TIME TO FEED");
+                 break; 
+        }  
+    }
+    else lcd.print("TIME TO FEED");
     // Display hour
     current_cursor = 5;
     lcd.setCursor(current_cursor, 1);       
@@ -76,35 +103,60 @@ void displayTimeSchedule_LCD(int hour, int minute, int second) {
     lcd.setCursor(++current_cursor, 1);
     lcd.print(second);
 }
+// Set max food per day
+void setMaxFood() {
+  lcd.init();
+  lcd.setCursor(0, 0);
+  lcd.print("MAX FOOD PER DAY");
+  lcd.setCursor(6,1);
+  MAX_FOOD_PER_DAY += 200;
+  lcd.print(MAX_FOOD_PER_DAY);
+}
+// Check if the button is holding more than 1s
+bool isHoldingMoreThan1s(int button) {
+  while (digitalRead(button) == HIGH) {
+    counter++;
+    if (counter >= 1000) {
+      counter = 0;
+      return true;
+    }
+  }
+  return false;
+}
+// Don't allow the user to set more food per day (reach max).
+void Exceeding() {
+  lcd.init();
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("CAN'T SET MORE");
+}
 
 void setup()
 {
     Serial.begin(115200);
     pinMode(btn[0], INPUT);
     pinMode(btn[1], INPUT);
-    MAX_FOOD_PER_DAY = 1000;
-    remaining_food = 100;
+    remaining_food = 0;
+    MAX_FOOD_PER_DAY = 0;
+    hour = 0, minute = 0, second = 0;
+    flag_longclick_btn1 = 0;
+    flag_settingSchedule = 0;
+    startMillis = millis();  // Initial start time
+    status = INIT_MODE;
 }
 
-bool CheckButtonIsHoldMoreThan1s(int button) {
-     if (digitalRead(button) == HIGH) counter++;
-     if (counter >= 1000) {
-        counter = 0;
-        return true;
-     }
-     return false;
-}
 void loop()
 {
-  long int timer0_millis = millis();
+  currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
   // Turn off LCD after 15s if nothing changes
-  if (timer0_millis >= 15000) {
-      lcd.noDisplay(); 
+  if (currentMillis - startMillis >= TIME_NOTHING_CHANGES) {
+      lcd.noBacklight();
+      lcd.noDisplay();
   }
-  
-  if (digitalRead(btn[0]) || digitalRead(btn[1]) == IS_PRESSED) timer0_millis = 0;
-  // Switching text if button 1 is pressed
+  if ((digitalRead(btn[0]) || digitalRead(btn[1]) || digitalRead(btn[2])) == IS_PRESSED) startMillis = millis();
+  // Switching text if button 0 is pressed
   if (digitalRead(btn[0]) == IS_PRESSED) {
+    flag_settingSchedule = 0;
     switch(mode) {
     case R_MODE: 
         displayRemainingFood_LCD();
@@ -119,54 +171,56 @@ void loop()
     }
   }
   // Setup schedule with button 2
+  // If you hold the button for at least 1 seconds, then set for an hour/minute/second depending on the current state.
+  if (isHoldingMoreThan1s) {
+    switch (status) {
+      case INIT_MODE:
+          status = H_MODE;
+          break;
+      case H_MODE:
+          status = M_MODE;
+          break;
+      case M_MODE:
+          status = S_MODE;
+          break;
+      case S_MODE:
+          status = H_MODE;
+          break;
+      default:
+          status = 0;     
+    }
+  }
   if (digitalRead(btn[1]) == IS_PRESSED) {
-      counter++;
-      // If you hold the button for at least 2 seconds, then set for an hour/minute/second depending on the current state.
-      if (counter >= 1000) {
-        counter = 0;
-        switch(status) {
-          case H_MODE:
-               while (!CheckButtonIsHoldMoreThan1s(btn[1])) {
-                   displayTimeSchedule_LCD(hour, minute, second);
-                   if (hour >= 24) hour = 0;
-                   else hour++;
-                   status = M_MODE;
-               }
-               break;
-          case M_MODE:
-               while (!CheckButtonIsHoldMoreThan1s(btn[1])) {
-                   displayTimeSchedule_LCD(hour, minute, second);
-                   if (minute >= 60) minute = 0;
-                   else minute++;
-                   status = S_MODE;
-               }
-               break;
-          case S_MODE:
-               while (!CheckButtonIsHoldMoreThan1s(btn[1])) {
-                   displayTimeSchedule_LCD(hour, minute, second);
-                   if (second >= 60) second = 0;
-                   else second++;
-                   status = H_MODE;
-               }
-               break;
-        }
-      }
-      switch(status) {
-          case H_MODE:
-               displayTimeSchedule_LCD(hour, minute, second);
-               if (hour >= 24) hour = 0;
-               else hour++;
-               break;
-          case M_MODE:
-               displayTimeSchedule_LCD(hour, minute, second);
-               if (minute >= 60) minute = 0;
-               else minute++;
-               break;
-          case S_MODE:
-               displayTimeSchedule_LCD(hour, minute, second);
-               if (second >= 60) second = 0;
-               else second++;
-               break;
-        }
+          flag_settingSchedule = 1;
+            switch(status) {
+              case H_MODE:
+                   if (digitalRead(btn[1]) == IS_PRESSED) {
+                       displayTimeSchedule_LCD(hour, minute, second);
+                       if (hour >= 24) hour = 0;
+                       else hour++;
+                   } 
+                   break;
+              case M_MODE:
+                   if (digitalRead(btn[1]) == IS_PRESSED) {
+                       displayTimeSchedule_LCD(hour, minute, second);
+                       if (minute >= 60) minute = 0;
+                       else minute++;
+                   }
+                   break;
+              case S_MODE:
+                  if (digitalRead(btn[1]) == IS_PRESSED) {
+                       displayTimeSchedule_LCD(hour, minute, second);
+                       if (second >= 60) second = 0;
+                       else second++;
+                  }
+                  status = H_MODE;
+                  break;
+          }
+  }
+  // Setup max food per day, with a max initial value of 0. To increase the 200, press once.
+  if (digitalRead(btn[2]) == IS_PRESSED) {
+      flag_settingSchedule = 0;
+      if (MAX_FOOD_PER_DAY < 1600) setMaxFood();
+      else Exceeding();
   }
 }
